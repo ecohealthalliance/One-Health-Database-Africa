b7f92cda7e9da59764fbd223e2c4f893e129a373
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2022-02-28 12:12:09 -0500

    function to scrape event data from who dashboard (issue #1)

---
+++ R_api/who_afro_report.R
@@ -0,0 +1,87 @@
+ingest_indicators.who_oubreaks <- function(){
+  
+  offset <- 0   # 2000 results returned at a time
+  events <- tibble()
+  
+  repeat {
+    
+    url <- paste0("https://services.arcgis.com/5T5nSi527N4F7luB/ArcGIS/rest/services/Join_Features_to_AFRO_COUNTRIES_BASIC3/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=",
+                  offset, "&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=")
+    out <- fromJSON(url)
+    features <- out$features
+    
+    if(is_empty(features)) break
+    
+    attributes <- features$attributes %>% 
+      as_tibble() %>% 
+      janitor::clean_names() %>% 
+      filter(type_of_event !=  "Humanitarian crises") %>% 
+      select(object_id, iso_3_code,who_code, country, event, 
+             date_notified_to_wco, start_of_reporting_period, end_of_reporting_period,
+             total_cases, cases_confirmed, deaths) %>%
+      mutate_at(.vars = c( "date_notified_to_wco", "start_of_reporting_period", "end_of_reporting_period"), 
+                ~lubridate::as_date(lubridate::as_datetime(as.numeric(str_sub(format(., scientific = FALSE), end = -4)))))
+    
+    events <- bind_rows(events, attributes)
+    
+    offset <- offset + 2000
+    
+  }
+  
+  # export full dataset
+  # write_csv(events, here::here("data", "who_outbreaks_full.csv"))
+  
+  # add field for length of reporting period
+  events2 <- events %>% 
+    group_by(country, event) %>% 
+    mutate(length_of_reporting_period_days = as.integer(end_of_reporting_period - start_of_reporting_period )) %>% 
+    ungroup()
+  
+  # id events with more than one start date
+  # events2 %>% 
+  #   group_by(country, event) %>% 
+  #   summarize(n = n_distinct(start_of_reporting_period)) %>% 
+  #   filter(n>1) %>% 
+  #   ungroup()
+  
+  # filter for most recent cumulative event
+  events3 <- events2 %>% 
+    group_by(country, event) %>% 
+    mutate(max_total_cases = max(total_cases)) %>% # for qa chack
+    filter(end_of_reporting_period == max(end_of_reporting_period)) %>% 
+    filter(total_cases == max(total_cases)) %>% # sometimes more than onee value reported for a given day
+    ungroup() %>% 
+    select(-object_id) %>% 
+    distinct()
+  
+  # id events where most recent isn't the greatest cumulative value
+  # events3 %>% 
+  #   filter(max_total_cases != total_cases)
+  
+  # id events with more than one date_notified_to_wco (but same end of reporting period)
+  # events3 %>% 
+  #   group_by(country, event) %>% 
+  #   summarize(n = n_distinct(date_notified_to_wco)) %>% 
+  #   ungroup()  %>% 
+  #   filter(n>1)
+  
+  # assume oldest date notified to wco
+  events4 <- events3 %>% 
+    group_by(country, event) %>% 
+    filter(date_notified_to_wco == min(date_notified_to_wco)) %>% 
+    ungroup()
+  
+  return(events4)
+  
+  # check there is one row for each event
+  # events4 %>% 
+  #   group_by(country, event) %>% 
+  #   summarize(n = n()) %>% 
+  #   ungroup() %>% 
+  #   distinct(n)
+  
+  # export condensed dataset
+  # write_csv(events, here::here("data", "who_outbreaks_compact.csv"))
+  
+  
+}
