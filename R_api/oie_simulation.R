403281e32f7ffbce1de1d772ee52f7cb1167f164
sebaum <baum@ecohealthalliance.org>
2022-07-13 18:57:53 -0400

    Adding OIE simulation exercises

---
+++ R_api/oie_simulation.R
@@ -0,0 +1,20 @@
+ingest_indicators.OIE_simulation <- function(){
+  read_xlsx("data/OIE_simulation_exercises.xlsx")  %>%
+    #select(all_of(jee_indicators)) %>%
+    mutate_if(is.character, as.factor) %>%
+    mutate(country = fct_recode(country, 
+                                #"Gambia" = "Gambia, The",
+                                #"Congo" = "Congo, Rep.",
+                                #"Democratic Republic of the Congo" = "Congo, Dem. Rep.", 
+                                "Eswatini" = "Swaziland")) %>%
+    filter(country %in% country_names) %>% 
+    droplevels() %>%
+    rename(year = "Year") %>%
+    pivot_longer(cols =!c(country, year), names_to = "indicator", values_to = "value") %>%
+    mutate(units = NA) %>%
+    droplevels() %>%
+    mutate(indicator = as.factor(indicator)) %>%
+    mutate(year = as.factor(year)) %>% 
+    relocate(country, indicator, year, value, units) %>%
+    set_names(colnames_list)
+}
