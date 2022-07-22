1e9ff9e1c962a6cd0e54cbbf2260a96ba8ffaa4e
sebaum <baum@ecohealthalliance.org>
2022-07-22 14:02:35 -0400

    revising - was calling sanitation, adding egypt

--- R_api/wash_hygiene_download.R
+++ R_api/wash_hygiene_download.R
@@ -1,15 +1,16 @@
 ingest_indicators.wash_hygiene_download <- function(){
   download.file("https://data.unicef.org/wp-content/uploads/2017/07/WASH-data-tables.xlsx",
                 destfile = "./temp.xlsx", mode = "wb")
-  hygiene <- read_xlsx("./temp.xlsx", sheet = 4 ) %>%
-    select(SANITATION...1, Year...2, NATIONAL...5) %>%
+  hygiene <- read_xlsx("./temp.xlsx", sheet = 6 ) %>%
+    select(HYGIENE, Year, NATIONAL) %>%
     slice(-c(1:2))  %>%
     set_names("country", "year", "hygiene_national_at_least_basic") %>%
     mutate_if(is.character, as.factor) %>%
     mutate(country =  stringi::stri_trans_general(str = country, id = "Latin-ASCII")) %>%
     mutate(country = fct_recode(country,
 #                                "Cote d'Ivoire" = "Côte d'Ivoire",
-                                "Tanzania" = "United Republic of Tanzania"  )) %>%
+                                "Tanzania" = "United Republic of Tanzania",
+                                "Egypt, Arab Rep." = "Egypt")) %>%
     filter(country %in% country_names) %>%
     droplevels()  %>%
     mutate(year = as.factor(year)) %>%
