5af4f0ddd6e86de875a4d1348d9d0c5f7d8a19eb
Collin Schwantes <schwantes@ecohealthalliance.org>
2022-08-01 11:20:19 -0500

    moved code and removed cleaning script + output

--- top_script_api.R
+++ top_script_api.R
@@ -123,6 +123,39 @@ full_data_number_com_api <- full_data_number_api %>%
 
 full_data_combined_api = rbind(full_data_factor_com_api, full_data_number_com_api)
 
-write.csv(full_data_combined_api, "Output/full_data_combined_api.csv", row.names = F)
+## make full data more readable and drop duplicate vet data ----
+
+# merge vet data from vet_capacity (oie_wahis) and Vets (combined_data_sheet)
+total_vets <- full_data_combined_api %>% 
+  filter(indicator == "Vets" | indicator == "vet_capacity") %>%
+  mutate(year = as.numeric(as.character(year))) %>% 
+  mutate(units = "Number") %>% 
+  group_by(country,year) %>% 
+  mutate(n = n()) %>% 
+  filter (n==1 | n==2 & !is.na(value)) %>% #Note: Duplicate for Sudan 2017 
+  select(-c(n)) %>% 
+  mutate(indicator = "total_vets") %>% 
+  distinct() 
+
+full_data_combined_api_clean <- rbind(full_data_combined_api, total_vets) %>% 
+  filter(indicator != "Vets" &  indicator != "vet_capacity")
+
+
+# rename SPAR labels
+full_data_combined_api_clean <- full_data_combined_api_clean  %>% 
+  mutate(indicator = fct_recode(indicator, 
+                                "C.3.1 Collaborative effort on activities to address zoonoses" = "C.3.1",
+                                "C.4.1 Multisectoral collaboration mechanism for food safety events" = "C.4.1",
+                                "C.5.1 Specimen referral and transport system" = "C.5.1", 
+                                "C.5.3 Access to laboratory testing capacity for priority diseases" = "C.5.3",
+                                "C.6.1 Early warning function: indicator- and event-based surveillance" = "C.6.1",
+                                "C.6.2 Mechanism for event management (verification, risk assessment, analysis investigation)" = "C.6.2",
+                                "C.11.1 Core capacity requirements at all times for designated airports, ports and ground crossings" = "C.11.1",
+                                "C.11.2 Effective public health response at points of entry" = "C.11.2")) 
+
+
+### write outputs ----
+
+write.csv(full_data_combined_api_clean, "Output/full_data_combined_api.csv", row.names = F)
 write.csv(full_data_number_api, "Output/full_data_number_api.csv", row.names = F)
 write.csv(full_data_factor_api, "Output/full_data_factor_api.csv", row.names = F)
