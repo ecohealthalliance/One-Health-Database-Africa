8d0c4ca095b47cf55c1d64e9a135e8172de69634
sebaum <baum@ecohealthalliance.org>
2022-09-10 12:48:30 -0400

    included both variables - all mammal species and mammal species excluding extinct, introduced, and vagrant

--- R_api/iucn_mammals.R
+++ R_api/iucn_mammals.R
@@ -1,14 +1,16 @@
 ingest_indicators.iucn_mammals <- function(){
- read_csv("data/mammals.csv") %>%
+ 
+read_csv("data/mammals.csv") %>%
     mutate(country = fct_recode (country,
                                  "Democratic Republic of the Congo" = "Congo, The Democratic Republic of the",
                                  "Egypt, Arab Rep." = "Egypt",
                                  "Tanzania" = "Tanzania, United Republic of")) %>%
     filter(country %in% country_names) %>%
-    rename(value = mammals_extinct_introduced_vagrant_removed) %>% 
-    select(-c(mammals)) %>% 
-    mutate(year = NA, 
-         indicator = "mammals_count",
-         units = "number")
+    pivot_longer(cols =!c(country), names_to = "indicator", values_to = "value") %>%
+    mutate(indicator = fct_recode(indicator, 
+                                  "mammal species_all"= "mammals", 
+                                  "mammal species_extinct_introduced_vagrant_removed" = "mammals_extinct_introduced_vagrant_removed")) %>% 
+    mutate(year = NA,
+           units = "number")
 }
   
